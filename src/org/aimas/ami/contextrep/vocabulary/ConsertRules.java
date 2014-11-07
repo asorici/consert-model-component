5af2ad7668de73806028a47de80a175ccf227b08
Alex Sorici <alex.sorici@gmail.com>
2014-11-07 16:42:00 +0200

    Adding the vocabulary referring to the DomainEntering, DomainLeaving detection rules.

--- src/org/aimas/ami/contextrep/vocabulary/ConsertRules.java
+++ src/org/aimas/ami/contextrep/vocabulary/ConsertRules.java
@@ -18,6 +18,21 @@ public class ConsertRules {
 	
     /** <p>The namespace of the vocabulary as a resource</p> */
     public static final Resource NAMESPACE = m_model.createResource( NS );
-	
-	public static final Property DERIVE_ASSERTION = ResourceFactory.createProperty(SPIN.NS + "deriveassertion"); 
+    
+    // Vocabulary classes
+    /////////////////////
+    public final static Resource DomainDetection = m_model.createResource( NS + "DomainDetection" );
+    public final static Resource EnteringDomainDetection = m_model.createResource( NS + "EnteringDomainDetection" );
+    public final static Resource LeavingDomainDetection = m_model.createResource( NS + "LeavingDomainDetection" );
+    
+    public final static Resource ContextDomainEnteredRule = m_model.createResource( NS + "ContextDomainEnteredRule" );
+    public final static Resource ContextDomainLeftRule = m_model.createResource( NS + "ContextDomainLeftRule" );
+    
+    // Vocabulary properties
+ 	////////////////////////
+    public static final Property hasDetectedDimension = ResourceFactory.createProperty(SPIN.NS + "hasDetectedDimension"); 
+    public static final Property hasDetectedDomainUser = ResourceFactory.createProperty(SPIN.NS + "hasDetectedDomainUser"); 
+    public static final Property hasDetectedDomainValue = ResourceFactory.createProperty(SPIN.NS + "hasDetectedDomainValue");
+    
+    public static final Property DERIVE_ASSERTION = ResourceFactory.createProperty(SPIN.NS + "deriveassertion"); 
 }
